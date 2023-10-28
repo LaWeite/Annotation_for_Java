bf8a9a99fc8767afd155b5eba0e2cc401626f160
LaWeite <96009716+LaWeite@users.noreply.github.com>
2023-10-28 13:47:29 +0300

    Create Replacing_digits.java

---
+++ functional/Replacing_digits.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
+class DigitsSubstitution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextInt();
+
+        int result = Utils.substitution(n, digit -> {
+            switch (digit) {
+                case 5:
+                    return 1;
+                case 6:
+                    return 2;
+                case 7:
+                    return 3;
+                case 8:
+                    return 4;
+                case 9:
+                    return 5;
+            }
+
+            return digit;
+        });
+
+        System.out.println(result);
+    }
+}
+
+class Utils {
+
+    public static int substitution(long n, Function<Integer, Integer> digitRule) {
+        String numberAsString = String.valueOf(n);
+        int result = 0;
+
+        for (int i = 0; i < numberAsString.length(); i++) {
+            result *= 10;
+            int nextDigit = Integer.parseInt(String.valueOf(numberAsString.charAt(i)));
+            result += digitRule.apply(nextDigit);
+        }
+
+        return result;
+    }
+}
