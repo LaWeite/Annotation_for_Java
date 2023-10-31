fe5fe993a618c64969947fdd19dc9164e3a85aca
Кручинин Евгений <96009716+LaWeite@users.noreply.github.com>
2023-10-31 11:05:42 +0300

    Create remove_element.java

---
+++ algorithms/remove_element.java
@@ -0,0 +1,11 @@
+public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int element : nums) {
+            if (element != val) {
+                nums[i] = element;
+                ++i;
+            }
+        }
+
+        return i + 1;
+    }
