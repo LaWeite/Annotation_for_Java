33814e469d63726da9b66f1320e0528065e71b20
Кручинин Евгений <96009716+LaWeite@users.noreply.github.com>
2023-11-01 12:24:22 +0300

    Update sqrt(x).java

--- algorithms/sqrt(x).java
+++ algorithms/sqrt(x).java
@@ -19,25 +19,18 @@ class Solution {
 //Second variant
 class Solution {
     public int mySqrt(int x) {
-        return findNumberOfSqrt(x);
-    }
-    public static int findNumberOfSqrt(int number){
-        int start = 0;
-        int end = number;
-        long mid = start + (end - start)/2;
-        int ans = -1;
-        while (start <= end) {
-            long sqrt = mid * mid;
-            if (number == sqrt) {
-                return (int)mid;
-            } else if(sqrt < number){
-                ans = (int)mid;
-                start = (int)mid + 1;
-            } else {
-                end = (int)mid-1;
+        int start=1;
+        int end=x;
+        while(start<=end){
+            
+           int mid=start+(end-start)/2;
+            if(mid<=x/mid){
+                start=mid+1;
             }
-            mid = start + (end - start)/2;
+            else{
+                end=mid-1;
+            }
         }
-        return ans;
-    } 
+        return end;
+    }
 }
